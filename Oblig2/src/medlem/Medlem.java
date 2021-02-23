1a9917db71ab1c63067d5350d678522f9db945fe
yfess <yfess@158.37.226.74>
2021-02-23 14:30:42 +0100

    medlem konstroktør

--- Oblig2/src/medlem/Medlem.java
+++ Oblig2/src/medlem/Medlem.java
@@ -2,10 +2,17 @@ package medlem;
 
 import hobby.Hobby;
 import no.hvl.dat102.mengde.adt.MengdeADT;
+import no.hvl.dat102.mengde.kjedet.KjedetMengde;
 
 public class Medlem {
 	private String navn;
 	private MengdeADT<Hobby> hobbyer;
 	private int statusIndeks;
+
+	public <T> Medlem() {
+		navn = null;
+		hobbyer = new KjedetMengde<Hobby>();
+		statusIndeks = 0;
+	}
 
 }
